a514022e8fbfe33d11b6efa4df71214f67bef2dd
Chris Sanders <sanders.chris@gmail.com>
2019-07-27 21:07:57 -0500

    Add hooks for setting up pod spec

--- lib/lib.py
+++ lib/lib.py
@@ -1,4 +1,5 @@
-from charmhelpers.core import hookenv
+from charmhelpers.core import hookenv, templating
+from charms import layer
 
 
 class ${libclass}():
@@ -8,3 +9,10 @@ class ${libclass}():
     def action_function(self):
         ''' An example function for calling from an action '''
         return
+
+    def make_pod_spec():
+        image_info = layer.docker_resource.get_info('${metadata.package}_image')
+        context = image_info
+        context['name'] = '${metadata.package}'
+        spec = templating.render('pod_spec.j2', None, context=context)
+        return spec
--- reactive/handlers.py
+++ reactive/handlers.py
@@ -1,24 +1,26 @@
 from ${libfile} import ${libclass}
 from charmhelpers.core import hookenv
-from charms.reactive import set_flag, when_not
+from charms.reactive import set_flag, when_not, when
+from charms import layer
 
 helper = ${libclass}()
 
 
-@when_not('${metadata.package}.installed')
-def install_${metadata.package.replace('-', '_')}():
-    #raw
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
-    #end raw
-    hookenv.status_set('active', '')
-    set_flag('${metadata.package}.installed')
+@when_not('layer.docker-resource.${metadata.package}_image.fetched')
+def fetch_image():
+    layer.docker_resource.fetch('${metadata.package}_image')
+
+@when('${metadata.package}.configured')
+def ${safe_package}_active():
+    hookenv.status_set('active','')
+
+@when('layer.docker-resource.${metadata.package}_image.fetched')
+@when_not('${metadata.package}.configured')
+def ${safe_package}_configure():
+    hookenv.status_set('maitenance', 'Configuring ${metadata.package} container')
+    spec = helper.make_pod_spec()
+    hookenv.log('Setting pod spec:\n{}'.format(spec))
+    if layer.caas_base.pod_spec_set(spec):
+        hookenv.set_flag('${metadata.package}.configured')
+    else:
+        hookenv.log('Failed to set spec', 'ERROR')
