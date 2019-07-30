db4f1bde79bc1dd0acd21c433dde15d21fdfddb1
Chris Sanders <sanders.chris@gmail.com>
2019-07-29 22:24:16 -0500

    Reduce timeout

--- tests/functional/test_deploy.py
+++ tests/functional/test_deploy.py
@@ -64,7 +64,7 @@ async def test_${fixture}_deploy(model, image, source, request):
 
 
 @pytest.mark.deploy
-@pytest.mark.timeout(300)
+@pytest.mark.timeout(60)
 async def test_charm_upgrade(model, app):
     if app.name.endswith("local"):
         pytest.skip("No need to upgrade the local deploy")
@@ -84,7 +84,7 @@ async def test_charm_upgrade(model, app):
 
 
 @pytest.mark.deploy
-@pytest.mark.timeout(300)
+@pytest.mark.timeout(60)
 async def test_${fixture}_status(model, app):
     # Verifies status for all deployed series of the charm
     await model.block_until(lambda: app.status == 'active')
